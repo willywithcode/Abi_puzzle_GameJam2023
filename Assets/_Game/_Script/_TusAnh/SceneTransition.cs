f2c5347ef9f089f75700a42cc06bd00368a706a4
Tran Anh Tu <85071582+tanhtuscode@users.noreply.github.com>
2023-09-30 15:56:08 +0700

    - add game ui

--- Assets/_Game/_Script/_TusAnh/SceneTransition.cs
+++ Assets/_Game/_Script/_TusAnh/SceneTransition.cs
@@ -1,12 +1,29 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneTransition : MonoBehaviour
 {
-    private void Start()
+    [SerializeField] private Animator _animatorSceneChange;
+    
+
+    public void Start()
     {
-        
+     
+    }
+    public void enableTransition()
+    {
+        _animatorSceneChange.SetTrigger("Trigger");
+        StartCoroutine(disabeTime());
+    }
+
+    private IEnumerator disabeTime()
+    {
+        yield return new WaitForSeconds(4f);
+        _animatorSceneChange.ResetTrigger("Trigger");
     }
 }
